8e7995b4e1add1765ff798eb50c63d7c51c000bf
Daniel Kurz <danielkurz03@protonmail.com>
2025-04-11 14:30:44 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vercel from '@astrojs/vercel';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
