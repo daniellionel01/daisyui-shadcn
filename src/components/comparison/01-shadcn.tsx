39d97998352dca632cf307368d12687b947c9d70
Daniel Kurz <danielkurz03@protonmail.com>
2025-04-11 14:07:38 +0200

    shadcn comparison 01

---
+++ src/components/comparison/01-shadcn.tsx
@@ -0,0 +1,63 @@
+import { CardsActivityGoal } from "./01-shadcn/activity-goal";
+import { CardsCalendar } from "./01-shadcn/calendar";
+import { CardsChat } from "./01-shadcn/chat";
+import { CardsCookieSettings } from "./01-shadcn/cookie-settings";
+import { CardsCreateAccount } from "./01-shadcn/create-account";
+import { CardsDataTable } from "./01-shadcn/data-table";
+import { CardsMetric } from "./01-shadcn/metric";
+import { CardsPaymentMethod } from "./01-shadcn/payment-method";
+import { CardsReportIssue } from "./01-shadcn/report-issue";
+import { CardsShare } from "./01-shadcn/share";
+import { CardsStats } from "./01-shadcn/stats";
+import { CardsTeamMembers } from "./01-shadcn/team-members";
+
+export function ComparisonShadcn01() {
+  return (
+    <div className="md:grids-col-2 grid md:gap-4 lg:grid-cols-10 xl:grid-cols-11 xl:gap-4">
+      <div className="space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
+        <CardsStats />
+        <div className="grid gap-1 sm:grid-cols-[260px_1fr] md:hidden">
+          <CardsCalendar />
+          <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-4">
+            <CardsActivityGoal />
+          </div>
+          <div className="pt-3 sm:col-span-2 xl:pt-4">
+            <CardsMetric />
+          </div>
+        </div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+          <div className="space-y-4 xl:space-y-4">
+            <CardsTeamMembers />
+            <CardsCookieSettings />
+            <CardsPaymentMethod />
+          </div>
+          <div className="space-y-4 xl:space-y-4">
+            <CardsChat />
+            <CardsCreateAccount />
+            <div className="hidden xl:block">
+              <CardsReportIssue />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-4 lg:col-span-6 xl:col-span-5 xl:space-y-4">
+        <div className="hidden gap-1 sm:grid-cols-[260px_1fr] md:grid">
+          <CardsCalendar />
+          <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-3">
+            <CardsActivityGoal />
+          </div>
+          <div className="pt-3 sm:col-span-2 xl:pt-3">
+            <CardsMetric />
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <CardsDataTable />
+        </div>
+        <CardsShare />
+        <div className="xl:hidden">
+          <CardsReportIssue />
+        </div>
+      </div>
+    </div>
+  );
+}
